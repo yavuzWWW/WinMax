3edb21bb549413d690af0da6a8af61b8984c000a
Yavuz <yavuzsemih.nl@gmail.com>
2026-08-24 01:43:25 +0200

    test: validate settings profile format in production gate

--- scripts/check.sh
+++ scripts/check.sh
@@ -27,6 +27,14 @@ xcrun swiftc \
   -o "$BUILD/versioning-tests"
 "$BUILD/versioning-tests"
 
+xcrun swiftc \
+  -swift-version 5 \
+  -warnings-as-errors \
+  "$ROOT/Sources/SettingsProfile.swift" \
+  "$ROOT/scripts/test-settings-profile.swift" \
+  -o "$BUILD/settings-profile-tests"
+"$BUILD/settings-profile-tests"
+
 "$ROOT/scripts/build.sh"
 
 APP="$BUILD/WinMax.app"
